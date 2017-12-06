34b6e567c01145df78f07cd76639c9ee11984f2a
Jim Yuan <aquarior@gmail.com>
2017-12-06 15:50:56 +0800

    修改了一点点 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
-// http://eslint.org/docs/user-guide/configuring
+// https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 6,
     sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
@@ -18,23 +17,9 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 0,
+    'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  },
-  "globals": {
-    "console": true,
-    "afterEach": false,
-    "beforeEach": false,
-    "describe": false,
-    "expect": false,
-    "it": false,
-    "require": false,
-    "process": false,
-    "Buffer": false,
-    "window": true
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
