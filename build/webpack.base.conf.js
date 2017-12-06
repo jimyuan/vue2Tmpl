9a9c8687ab06b903211896f9782b4517a8c36ab5
Jim Yuan <aquarior@gmail.com>
2017-12-06 11:41:21 +0800

    BREAKING: update to webpack 3

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,17 +1,30 @@
-var path = require('path')
-var utils = require('./utils')
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
-var hashMark = config.build.hashMark
-var hash = hashMark ? '.[hash:' + hashMark + ']' : ''
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
+const hashMark = config.build.hashMark
+const hash = hashMark ? '.[hash:' + hashMark + ']' : ''
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
+const createLintingRule = () => ({
+  test: /\.(js|vue)$/,
+  loader: 'eslint-loader',
+  enforce: 'pre',
+  include: [resolve('src'), resolve('test')],
+  options: {
+    formatter: require('eslint-friendly-formatter'),
+    emitWarning: !config.dev.showEslintErrorsInOverlay
+  }
+})
+
 module.exports = {
+  context: path.resolve(__dirname, '../'),
   entry: {
-    app: ['./src/main.js']
+    app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -22,12 +35,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
-    modules: [
-      resolve('src'),
-      resolve('node_modules')
-    ],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
       'SRC': resolve('src'),
       'ASSETS': resolve('src/assets'),
       'COMPONENTS': resolve('src/components'),
@@ -40,15 +50,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: "pre",
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
@@ -59,25 +60,41 @@ module.exports = {
         include: [resolve('src'), resolve('test')]
       },
       {
-        test: /\.scss$/,
-        loaders: ['vue-style-loader', 'css-loader', 'sass-loader']
-      },
-      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
-          limit: 1,
-          name: utils.assetsPath('img/[name]' + hash + '.[ext]')
+        options: {
+          limit: 10000,
+          name: utils.assetsPath(`img/[name]${hash}.[ext]`)
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath(`media/[name]${hash}.[ext]`)
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name]' + hash + '.[ext]')
+          name: utils.assetsPath(`font/[name]${hash}.[ext]`)
         }
       }
     ]
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   }
 }
